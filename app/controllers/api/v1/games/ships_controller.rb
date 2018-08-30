8583953d394af5be77290b66ad633f47f4219ec9
Emmie Hayes <emmiehayes2@gmail.com>
2018-08-30 17:06:14 -0600

    WIP moves into ships_controller

--- app/controllers/api/v1/games/ships_controller.rb
+++ app/controllers/api/v1/games/ships_controller.rb
@@ -1,4 +1,19 @@
-class ShipsController < ApplicationController
-  def create 
+module Api
+  module V1
+    module Games
+      class ShipsController < ApiController
+        def create 
+=begin
+          define the board 
+          :Parameters {"ship_size"=>3, 
+                        "start_space"=>"A1", 
+                        "end_space"=>"A3", 
+                        "controller"=>"api/v1/games/ships", 
+                        "action"=>"create", "game_id"=>"32", 
+                        "ship"=>{"ship_size"=>3, "start_space"=>"A1", "end_space"=>"A3"}} permitted: false>
+=end
+        end
+      end 
+    end 
   end
 end
