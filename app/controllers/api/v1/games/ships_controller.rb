8caa1c788d7e43ff9e77934f62196b475a6b1127
Emmie Hayes <emmiehayes2@gmail.com>
2018-08-30 19:00:54 -0600

    working through spec harness, leaving off with message handling

--- app/controllers/api/v1/games/ships_controller.rb
+++ app/controllers/api/v1/games/ships_controller.rb
@@ -1,13 +1,10 @@
-module Api
-  module V1
-    module Games
-      class ShipsController < ApiController
-        def create 
-          game = Game.find(params[:game_id].to_i)
-          game.set_starter
-          render json: game
-        end
-      end 
-    end
+class Api::V1::Games::ShipsController < ApiController
+  
+  def create 
+    game = Game.find(params[:game_id].to_i)
+    
+    game.set_starter
+    
+    render json: game
   end
-end
+end 
