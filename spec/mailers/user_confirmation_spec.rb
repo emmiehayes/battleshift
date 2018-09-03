56cd3b32a1dc95ec4e92abf152a428ab9634d5bd
Emmie Hayes <emmiehayes2@gmail.com>
2018-09-02 18:06:34 -0600

    add mailer test

--- spec/mailers/user_confirmation_spec.rb
+++ spec/mailers/user_confirmation_spec.rb
@@ -1,5 +1,22 @@
 require "rails_helper"
 
 RSpec.describe UserConfirmationMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'confirmation' do
+  let(:user) { mock_model User, id: 1, name: 'test', email: 'test@gmail.com', password_digest: 'rrtgf', email_confirmed: false, email_token: 'erthgf', api_key: 'aterhgf' }
+  let(:mail) { UserConfirmationMailer.confirmation(user).deliver_now }
+
+    it 'renders the subject' do
+      expect(mail.subject).to eq('Activate your account')
+    end
+
+    it 'renders the receiver email' do
+      expect(mail.to).to eq([user.email])
+    end
+
+    it 'renders the api_key' do
+      expect(mail.body.encoded).to have_content("Your API Key: #{user.api_key}")
+      expect(mail.body.encoded).to have_content("Thanks for signing up, #{user.name}!")
+    end
+  end
 end
+
